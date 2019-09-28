2e70d206f40051882bf0e3de49f5667e291389bc
Cody P Schafer <dev@codyps.com>
2019-09-28 14:10:37 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+
+// accept adapter/frontend spec
+// accept tuning parameters (consider dvbv5 config file support)
+// dvb tune/configure
+// read mpegts stream
+// split mpegts stream on appropriate boundaries at times specified
+// maintain dvb tuning
+
+fn main() {
+
+    println!("Hello, world!");
+}
